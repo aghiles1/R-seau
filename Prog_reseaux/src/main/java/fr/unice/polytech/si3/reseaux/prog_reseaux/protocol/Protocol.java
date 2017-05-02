dc47747a7081c94b913b4354a9c91a585afded32
Elie Baroghel <elie.baroghel@etu.unice.fr>
2017-05-02 14:24:51 +0100

    grgr

---
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/protocol/Protocol.java
@@ -0,0 +1,12 @@
+package fr.unice.polytech.si3.reseaux.prog_reseaux.protocol;
+
+/**
+ * Created by User on 02/05/2017.
+ */
+public class Protocol {
+    public static final String ADD = "ADD";
+    public static final String LIST = "LIST";
+    public static final String JOIN = "JOIN";
+    public static final String STUDENTS = "STUDENTS";
+    public static final String ERROR = "ERROR";
+}
