b05c732d8665386851ce5e89fdd9596d912ff832
Elie Baroghel <elie.baroghel@etu.unice.fr>
2017-05-02 14:06:58 +0100

    package

---
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/protocol/Requete.java
@@ -0,0 +1,11 @@
+package fr.unice.polytech.si3.reseaux.prog_reseaux.protocol;
+
+/**
+ * Created by User on 02/05/2017.
+ */
+public class Requete {
+    private String type;
+    private Object data;
+
+    
+}
