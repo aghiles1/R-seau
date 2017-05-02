7d4f9d944e02deae3c632588314491ecde327e7d
Rachida ZONGO <rachida.zongo@etu.unice.fr>
2017-05-02 13:56:51 +0200

    MAVEN Project <3 <3 <3

---
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/MainApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.unice.polytech.si3.reseaux.prog_reseaux;
+
+/**
+ *
+ * @author rash
+ */
+public class MainApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
