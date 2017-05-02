408bd94388c201d8e4b6169f701bce647ecafcd5
Elie Baroghel <elie.baroghel@etu.unice.fr>
2017-05-02 14:09:35 +0100

    protocol txt

--- Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/protocol/Requete.java
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/protocol/Requete.java
@@ -7,5 +7,24 @@ public class Requete {
     private String type;
     private Object data;
 
-    
+    public Requete(String type, Object data){
+        this.type = type;
+        this.data = data;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return "Requete{" +
+                "type='" + type.toString() + '\'' +
+                ", data=" + data.toString() +
+                '}';
+    }
 }
