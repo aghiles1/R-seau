70abdd572d6d2922d5c5007979b6a31355fe04f1
Dylan Ritrovato <dylan-jonathan.ritrovato@etu.unice.fr>
2017-05-07 11:03:21 +0200

    prtrsrtdsktp

--- Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Client.java
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Client.java
@@ -1,8 +1,27 @@
 package fr.unice.polytech.si3.reseaux.prog_reseaux;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * @author 
  */
 public class Client {
+	Socket client;
+	
+	public Client(){
+		try {
+			client = new Socket(Configuration.NOM_MACHINE, Configuration.NUM_PORT);
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
     
 }
--- Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Serveur.java
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Serveur.java
@@ -1,8 +1,29 @@
 package fr.unice.polytech.si3.reseaux.prog_reseaux;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
- * @author 
+ * @author Azu-Chan
  */
 public class Serveur {
+    ServerSocket serv;
+    Socket client;
     
+    public Serveur(){
+    	try {
+			serv = new ServerSocket(Configuration.NUM_PORT);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+    	
+    	try {
+			client = serv.accept();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+    }
 }
