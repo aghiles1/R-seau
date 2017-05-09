65c2ca7c1c59c2a36756454437ae9aac83780760
aghiles dziri <aghiles dziri>
2017-05-09 13:39:50 +0200

    sqd

--- Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Client.java
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Client.java
@@ -5,12 +5,14 @@
  */
 package fr.unice.polytech.si3.reseaux.prog_reseaux;
 
-import fr.unice.polytech.si3.reseaux.prog_reseaux.protocol.Protocol;
-import fr.unice.polytech.si3.reseaux.prog_reseaux.protocol.Requete;
+import fr.unice.polytech.si3.reseaux.prog_reseaux.protocol.*;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -18,24 +20,48 @@ import java.net.Socket;
  */
 public class Client {
 
+
+    public static void main(String[] s){
+        new Client().start(s[0], Integer.valueOf(s[1]),s[2]);
+    }
     public Client(){
 
     }
 
-    public void start(String server, int port){
+    public void start(String server, int port, String type){
+        Scanner sc  = new Scanner(System.in);
+        Object object = null;
+        switch (type.toLowerCase()){
+            case "add" :
+                System.out.println("nom etudian et l'email\n");
+                Etudiant etudiant = new Etudiant(sc.next(),sc.next());
+                System.out.println("libelle de l'idee\n");
+                String libelle = sc.next();
+                System.out.println("description de l'idee\n");
+                String description = sc.next();
+                object= new Idee(etudiant,libelle,description);
+                break;
+            case "list":
+                object = null;
+                break;
+
+
+        }
         try{
-            Requete requete = new Requete(Protocol.ADD, null);
+            Requete requete = new Requete(type,object );
 
             Socket socketConnection = new Socket(server, port);
+
             ObjectOutputStream clientOutputStream =
                     new ObjectOutputStream(socketConnection.getOutputStream());
+
             ObjectInputStream clientInputStream = new
                     ObjectInputStream(socketConnection.getInputStream());
 
             clientOutputStream.writeObject(requete);
 
             Requete reponse = (Requete)clientInputStream.readObject();
-
+            System.out.println(reponse.toString());
             clientInputStream.close();
             clientOutputStream.close();
         }catch (Exception e){
