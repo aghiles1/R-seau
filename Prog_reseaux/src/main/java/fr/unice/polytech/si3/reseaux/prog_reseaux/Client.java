6e4a7233d3bfdc8e55cea523c17ee6a41318cfed
Dylan Ritrovato <dylan-jonathan.ritrovato@etu.unice.fr>
2017-05-09 12:34:13 +0200

    aghiles commit

--- Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Client.java
+++ Prog_reseaux/src/main/java/fr/unice/polytech/si3/reseaux/prog_reseaux/Client.java
@@ -1,29 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package fr.unice.polytech.si3.reseaux.prog_reseaux;
 
-import java.io.IOException;
+import fr.unice.polytech.si3.reseaux.prog_reseaux.protocol.Protocol;
+import fr.unice.polytech.si3.reseaux.prog_reseaux.protocol.Requete;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
- * @author 
+ *
+ * @author user
  */
 public class Client {
-	Socket client;
-	
-	public Client(){
-		try {
-			client = new Socket(Configuration.NOM_MACHINE, Configuration.NUM_PORT);
-			
-			client.close();
-		} catch (UnknownHostException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-		
-		
-	}
-    
+
+    public Client(){
+
+    }
+
+    public void start(String server, int port){
+        try{
+            Requete requete = new Requete(Protocol.ADD, null);
+
+            Socket socketConnection = new Socket(server, port);
+            ObjectOutputStream clientOutputStream =
+                    new ObjectOutputStream(socketConnection.getOutputStream());
+            ObjectInputStream clientInputStream = new
+                    ObjectInputStream(socketConnection.getInputStream());
+
+            clientOutputStream.writeObject(requete);
+
+            Requete reponse = (Requete)clientInputStream.readObject();
+
+            clientInputStream.close();
+            clientOutputStream.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
